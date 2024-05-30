3bc9cfef173e4cd4237c5c42897c801b3c3bb3a4
Navneet <navneetshahi345@gmail.com>
2024-05-30 23:35:00 +0530

    added a dialog box

--- src/app/(root)/(routes)/page.tsx
+++ src/app/(root)/(routes)/page.tsx
@@ -1,10 +1,20 @@
+"use client";
+
+import Model from "@/components/model";
 import { UserButton } from "@clerk/nextjs";
 import React from "react";
 
 export default function page() {
   return (
     <div>
-      Hello <UserButton afterSignOutUrl="/" />
+      <Model
+        title="Create Store"
+        description="This is the store"
+        isOpen
+        onClose={() => {}}
+      >
+        This is the store model
+      </Model>
     </div>
   );
 }
---
+++ src/components/model.tsx
@@ -0,0 +1,37 @@
+"use client";
+
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+} from "@/components/ui/dialog";
+
+interface ModelProps {
+  title: string;
+  description: string;
+  isOpen: boolean;
+  onClose: () => void;
+  children?: React.ReactNode;
+}
+
+export default function Model({
+  title,
+  description,
+  isOpen,
+  onClose,
+  children,
+}: ModelProps) {
+  return (
+    <Dialog open={isOpen}>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>{title}</DialogTitle>
+          <DialogDescription>{description}</DialogDescription>
+        </DialogHeader>
+        <div>{children}</div>
+      </DialogContent>
+    </Dialog>
+  );
+}
