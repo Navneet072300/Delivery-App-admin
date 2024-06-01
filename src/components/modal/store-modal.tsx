de9b9e1867c335f91484f698e2bda4326226a04a
Navneet <navneetshahi345@gmail.com>
2024-06-01 15:50:23 +0530

    added form to create store and button

--- src/components/modal/store-modal.tsx
+++ src/components/modal/store-modal.tsx
@@ -1,21 +1,87 @@
 "use client";
 
-import React from "react";
+import React, { useState } from "react";
 import Model from "@/components/model";
 import { useStoreModel } from "@/hooks/user-store-model";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+
+const formSchema = z.object({
+  name: z
+    .string()
+    .min(3, { message: "Store name should be minium 3 characters" }),
+});
 
 export const StoreModal = () => {
   const storeModal = useStoreModel();
+
+  const [isLoading, setIsLoading] = useState(false);
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: "",
+    },
+  });
+
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    console.log(values);
+  };
   return (
-    <div>
-      <Model
-        title="Create Store"
-        description="This is the store"
-        isOpen={storeModal.isOpen}
-        onClose={storeModal.onClose}
-      >
-        Form to create this store
-      </Model>
-    </div>
+    <Model
+      title="Create a new Store"
+      description="Add a new store to manage the products and categories"
+      isOpen={storeModal.isOpen}
+      onClose={storeModal.onClose}
+    >
+      <div>
+        <div className=" space-y-4 py-2 pb-4">
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)}>
+              <FormField
+                control={form.control}
+                name="name"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Name</FormLabel>
+                    <FormControl>
+                      <Input
+                        disabled={isLoading}
+                        placeholder="Your store name"
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <div className=" pt-6 space-x-2 flex items-center justify-end w-full">
+                <Button
+                  disabled={isLoading}
+                  type="button"
+                  variant={"outline"}
+                  size={"sm"}
+                >
+                  Cancel
+                </Button>
+                <Button disabled={isLoading} type="submit" size={"sm"}>
+                  Continue
+                </Button>
+              </div>
+            </form>
+          </Form>
+        </div>
+      </div>
+    </Model>
   );
 };
