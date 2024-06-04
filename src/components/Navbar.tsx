306f40e5ef91501c8cee7639aed209d434294faa
Navneet <navneetshahi345@gmail.com>
2024-06-04 23:30:33 +0530

    switch 2

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,12 +1,33 @@
 import { UserButton } from "@clerk/nextjs";
 import MainNav from "./MainNav";
-import StoreSwitcher from "./StoreSwitcher";
+import { StoreSwitcher } from "./StoreSwitcher";
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
+import { collection, getDocs, query, where } from "firebase/firestore";
+import { db } from "@/lib/firebase";
+import { Store } from "@/type-db";
 
-const Navbar = () => {
+const Navbar = async () => {
+  const { userId } = auth();
+
+  if (!userId) {
+    redirect("/sign-in");
+  }
+
+  const storeSnap = await getDocs(
+    query(collection(db, "stores"), where("userId", "==", userId))
+  );
+
+  let stores = [] as Store[];
+
+  storeSnap.forEach((doc) => {
+    stores.push(doc.data() as Store);
+  });
+
   return (
     <div className=" border-b">
       <div className=" flex h-16 items-center px-4">
-        <StoreSwitcher items={[]} />
+        <StoreSwitcher items={stores} />
 
         {/* routes */}
         <MainNav />
--- src/components/StoreSwitcher.tsx
+++ src/components/StoreSwitcher.tsx
@@ -1,9 +1,24 @@
 "use client";
 
-import { PopoverTrigger } from "@/components/ui/popover";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
 import { Store } from "@/type-db";
 import { useParams, useRouter } from "next/navigation";
 import { useState } from "react";
+import { Button } from "@/components/ui/button";
+import { Check, ChevronsUpDown } from "lucide-react";
+import {
+  Command,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+} from "@/components/ui/command";
+import { cn } from "@/lib/utils";
 
 type PopoverTriggerProps = React.ComponentPropsWithoutRef<
   typeof PopoverTrigger
@@ -17,12 +32,12 @@ export const StoreSwitcher = ({ items }: StoreSwitcherPorps) => {
   const params = useParams();
   const router = useRouter();
 
-  const formattedStores = items.map((item) => ({
+  const formattedStores = items?.map((item) => ({
     label: item.name,
     value: item.id,
   }));
 
-  const currentStore = formattedStores.find(
+  const currentStore = formattedStores?.find(
     (item) => item.value === Number(params.storeId)
   );
 
@@ -34,8 +49,54 @@ export const StoreSwitcher = ({ items }: StoreSwitcherPorps) => {
   };
 
   return (
-    <div>
-      <h1>StoreSwitcher</h1>
-    </div>
+    <>
+      <Popover open={open} onOpenChange={setOpen}>
+        <PopoverTrigger asChild>
+          <Button
+            variant="outline"
+            role="combobox"
+            aria-expanded={open}
+            className="w-[200px] justify-between"
+          >
+            {currentStore?.value
+              ? formattedStores?.find(
+                  (framework) => framework.value === currentStore?.value
+                )?.label
+              : "Select store..."}
+            <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
+          </Button>
+        </PopoverTrigger>
+        <PopoverContent className="w-[200px] p-0">
+          <Command>
+            <CommandInput placeholder="Search store..." />
+            <CommandEmpty>No store found.</CommandEmpty>
+            <CommandList>
+              <CommandGroup>
+                {formattedStores?.map((store) => (
+                  <CommandItem
+                    key={store.value}
+                    value={store.value.toString()}
+                    onSelect={(currentValue) => {
+                      //setValue(currentValue === value ? "" : currentValue)
+                      setOpen(false);
+                    }}
+                  >
+                    <Check
+                      className={cn(
+                        "mr-2 h-4 w-4",
+                        currentStore?.value === store.value
+                          ? "opacity-100"
+                          : "opacity-0"
+                      )}
+                    />
+                    {store.label}
+                  </CommandItem>
+                ))}
+              </CommandGroup>
+            </CommandList>
+          </Command>
+        </PopoverContent>
+      </Popover>
+    </>
   );
 };
