7331848dea560066095c8b559515f12393f2de5f
Navneet <navneetshahi345@gmail.com>
2024-06-05 23:50:06 +0530

    made changes in store setter

--- src/components/StoreSwitcher.tsx
+++ src/components/StoreSwitcher.tsx
@@ -9,7 +9,7 @@ import { Store } from "@/type-db";
 import { useParams, useRouter } from "next/navigation";
 import { useState } from "react";
 import { Button } from "@/components/ui/button";
-import { Check, ChevronsUpDown } from "lucide-react";
+import { Check, ChevronsUpDown, StoreIcon } from "lucide-react";
 import {
   Command,
   CommandEmpty,
@@ -42,10 +42,19 @@ export const StoreSwitcher = ({ items }: StoreSwitcherPorps) => {
   );
 
   const [open, setOpen] = useState(false);
+  const [searchTerm, setSearchTerm] = useState("");
+  const [filtered, setFiltered] = useState<{label: string, value: string}[]>([]);
 
   const onStoreSelect = (store: { value: string; label: string }) => {
     setOpen(false);
     router.push(`${store.value}`);
+  };
+
+  const handleSearchTerm = (e: any) => {
+    setSearchTerm(e.target.value);
+    setFiltered(
+      formattedStores.filter(item => item.label.toLowerCase().includes(searchTerm.toLowerCase()))
+    )
   };
 
   return (
@@ -58,6 +67,7 @@ export const StoreSwitcher = ({ items }: StoreSwitcherPorps) => {
             aria-expanded={open}
             className="w-[200px] justify-between"
           >
+            <StoreIcon className=" mr-2 h-4 w-4" />
             {currentStore?.value
               ? formattedStores?.find(
                   (framework) => framework.value === currentStore?.value
@@ -68,29 +78,19 @@ export const StoreSwitcher = ({ items }: StoreSwitcherPorps) => {
         </PopoverTrigger>
         <PopoverContent className="w-[200px] p-0">
           <Command>
-            <CommandInput placeholder="Search store..." />
-            <CommandEmpty>No store found.</CommandEmpty>
+            <div className=" w-full px-2 py-1 flex items-center border rounded-md border-gray-100">
+              <StoreIcon className=" mr-2 h-4 w-4 min-w-4" />
+              <input
+                type="text"
+                placeholder=" Search Store.."
+                onChange={handleSearchTerm}
+                className=" flex-1 w-full outline-none"
+              />
+            </div>
             <CommandList>
-              <CommandGroup>
+              <CommandGroup heading="Stores">
                 {formattedStores?.map((store) => (
-                  <CommandItem
-                    key={store.value}
-                    value={store.value.toString()}
-                    onSelect={(currentValue) => {
-                      //setValue(currentValue === value ? "" : currentValue)
-                      setOpen(false);
-                    }}
-                  >
-                    <Check
-                      className={cn(
-                        "mr-2 h-4 w-4",
-                        currentStore?.value === store.value
-                          ? "opacity-100"
-                          : "opacity-0"
-                      )}
-                    />
-                    {store.label}
-                  </CommandItem>
+                  
                 ))}
               </CommandGroup>
             </CommandList>
